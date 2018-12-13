4f4b50928916bd45c3703ea65f00fb45eaf39a41
Mudit Srivastava (Digital) <mu242813@wipro.com>
2018-12-13 18:54:06 +0000

    printing array

---
+++ site_map.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen as uReq
+
+#import urllib
+import re
+ 
+def getLinks(url):
+    html_page = uReq(url)
+    soup = BeautifulSoup(html_page)
+    links = []
+ 
+    for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
+        links.append(link.get('href'))
+ 
+    return links
+links=[] 
+links=getLinks("https://wiprodigital.com")
+
+print(links)
+
